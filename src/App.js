7ac4de2886b0cd0da93204121f5e484c23aa2d9c
Estanislao Rey <tankysoluciones@gmail.com>
2022-10-13 21:12:21 -0300

    react

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const onHandleClick () =>{
+      console.log ("hubo clic")
+    }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HOLA MUNDO
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={} className='boton_primario'>CLIC AQUI</button>
       </header>
     </div>
   );
