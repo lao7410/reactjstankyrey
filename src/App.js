b20a51b97b997211a7013847155f666c63bd60f4
Estanislao Rey <tankysoluciones@gmail.com>
2022-10-20 20:48:19 -0300

    1 antes de preentrega

--- src/App.js
+++ src/App.js
@@ -1,18 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
 
+const products = [
+  {
+    id: 1,
+    name: 'Product 1',
+    price: 100,
+    description: 'description',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbN4hEMNReLTBOTFa8iKdx7JSM2EJ5ZPdfJCzu0CGY_H7UDX33I8GXQil_gkw0FqmHUMY&usqp=CAU'
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    price: 200,
+    description: 'description',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqIfPm1qtVRH2dz-EMrRc3l7r8kf-mVcQfXinzXrh992OgCQD1xmpN19C0kbgYJ2YwbxY&usqp=CAU'
+  },
+  {
+    id: 3,
+    name: 'Product 3',
+    price: 300,
+    description: 'description',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-Do-eg663j3L9FuvvV8--uvnknGCUVoBE1tmLLOnST65uTF4Z_StK7dTZ2pXe0wLLReY&usqp=CAU'
+  },
+]
+/* class ButtonClass extends React.Component{
+  constructor (props){
+    super(props)
+  }
+  render(){
+    return(
+      zbutton onclick
+    )
+  }
+} */
+
+ const Button = (props) => {
+  
+  return (
+    <button onClick={props.onClick} className='button-primary'>{props.text}</button>
+  )
+} 
+
 function App() {
-    const onHandleClick () =>{
-      console.log ("hubo clic")
-    }
+  const onHandleClick = () => {
+    console.log('was clicked')
+  }
+
+  const onHandlerAddProduct = () => {
+    console.log('add product')
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HOLA MUNDO
+          Hello World!
         </p>
-        <button onClick={} className='boton_primario'>CLIC AQUI</button>
+        <Button onClick={onHandleClick} text='Click me' />
+        <div className='products'>
+          {products.map((product) => (
+            <div key={product.id}>
+              <h3>{product.name}</h3>
+              <img className='product-image' src={product.imageUrl} alt={product.name} />
+              <p>{product.description}</p>
+              <Button onClick={onHandlerAddProduct} text='Add to Cart' />
+            </div>
+          ))}
+        </div>
       </header>
     </div>
   );
